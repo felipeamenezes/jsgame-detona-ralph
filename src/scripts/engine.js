fb2d9eedf54dd3ae3c44d35a9583d1d50dfedf85
Felipe Menezes <felipeamenezes@hotmail.com>
2024-02-29 09:26:52 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -9,7 +9,7 @@ const state = {
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
-    curretTime: 60,
+    currentTime: 60,
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
@@ -18,10 +18,10 @@ const state = {
 };
 
 function countDown() {
-  state.values.curretTime--;
-  state.view.timeLeft.textContent = state.values.curretTime;
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
 
-  if (state.values.curretTime <= 0) {
+  if (state.values.currentTime <= 0) {
     clearInterval(state.actions.countDownTimerId);
     clearInterval(state.actions.timerId);
     alert("Game Over! O seu resultado foi: " + state.values.result);
